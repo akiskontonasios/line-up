bdff34d3c4977ecd42f0b003b090a1e98b3ad3a9
Markus Kahl <machisuji@googlemail.com>
2013-06-19 13:30:00 +0100

    created first small corpus

--- src/main/scala/opus-europarl3.scala
+++ src/main/scala/opus-europarl3.scala
@@ -1,28 +1,88 @@
 package opus
 
-import scalaj.http.Http
 import sys.process._
+import xml._
+import java.net._
+import java.io._
+import java.util.zip.GZIPInputStream
+
+case class Translation(de: Seq[String], en: Seq[String]) {
+  override def toString =
+    "\nde %2d: %s\nen %2d: %s".format(de.size, de.mkString(" | "), en.size, en.mkString(" | "))
+}
 
 object Europarl3 {
-  val link = "<a[^/]*</a>".r
+
+  def bodyTag(name: String) = s"<$name[^/]*</$name>".r
+  def emptyTag(name: String) = s"<$name[^>]*>".r
+
+  val link = bodyTag("a")
   val href = "\"([^\"]*)\"".r
 
-  def url(lang: String) = "http://opus.lingfil.uu.se/Europarl3/xml/%s/" format lang 
-  def markup(lang: String) = Http(url(lang)).asString
-  def fetchFiles(lang: String) = link.findAllIn(markup(lang)).
+  val baseUrl = "http://opus.lingfil.uu.se/Europarl3/xml"
+  def url(lang: String): String = s"$baseUrl/$lang/"
+
+  def markup(lang: String): String = readURL(url(lang), in => io.Source.fromInputStream(in).mkString)
+
+  def fetchFiles(lang: String): Iterator[String] = link.findAllIn(markup(lang)).
     filter(_.contains(".xml.gz")).flatMap(link => href.findAllIn(link).map(_.replace("\"", "")))
 
-  object files {
-    lazy val de = fetchFiles("de").toList
-    lazy val en = fetchFiles("en").toList
-
-    lazy val de_en = de intersect en
+  def readURL[R](url: String, read: InputStream => R): R = {
+    val in = new BufferedInputStream(new URL(url).openStream());
+    try {
+      read(in)
+    } finally {
+      if (in ne null) {
+        try {
+          in.close()
+        } catch {
+          case e: IOException => {}
+        }
+      }
+    }
   }
 
-  def download() {
-    files.de_en.take(3).foreach { file =>
-      Seq("de", "en").foreach(lang =>
-        "wget -q %s%s -O %s".format(url(lang), file, file.replace(".xml.gz", s".$lang.xml.gz")).!)
+  lazy val files = fetchFiles("de-en").map(file => url("de-en") + file).toList
+
+  def translations(file: String): Seq[Translation] = {
+    val doc = readURL(file, in => io.Source.fromInputStream(new GZIPInputStream(in)).mkString)
+    val Attr = ".*\"(.*)\".*\"(.*)\".*\"(.*)\".*".r
+    val Targets = "<link.*\"([\\d ]+);([\\d ]+)\".*/>".r
+
+    val Attr(target, deFile, enFile) = emptyTag("linkGrp").findFirstIn(doc).get
+    val Seq(deSentences, enSentences) = Seq(deFile, enFile).map(file => sentences(s"$baseUrl/$file").toMap)
+
+    Targets.findAllIn(doc).map { link =>
+      val Targets(de, en) = link
+      Translation(
+        de.split(" ").map(_.toInt).map(i => deSentences(i)),
+        en.split(" ").map(_.toInt).map(i => enSentences(i)))
+    }.toSeq
+  }
+
+  def sentences(url: String): Seq[(Int, String)] = {
+    val doc = readURL(url, in =>
+      XML.loadString(io.Source.fromInputStream(new GZIPInputStream(in)).getLines.drop(1).mkString))
+    doc \\ "s" map (s => s.attribute("id").get.map(_.text.toInt).head ->
+      (s \\ "w").map(w => w.text).mkString("", " ", "").replaceAll(" ([\\.,!'])", "$1"))
+  }
+
+  def saveCorpus(fileName: String = "europarl3.txt", directory: String = ".", numberOfTexts: Int = -1) = {
+    val texts = if (numberOfTexts == -1) files.size else numberOfTexts
+    val out = new PrintWriter(new FileWriter(s"$directory/$fileName"))
+
+    try {
+      out.println("# number of included episodes: " + numberOfTexts)
+      out.println("# source: " + baseUrl + "/")
+      val numTranslations = files.take(texts).map(translations).map { translations =>
+        translations.foreach(out.println)
+        out.println("# next episode \n")
+        translations.size
+      }.sum
+
+      println(s"Saved corpus including $numberOfTexts episodes with $numTranslations translations.")
+    } finally {
+      out.close()
     }
   }
 }
